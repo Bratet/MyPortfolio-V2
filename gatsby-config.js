fbf04a5829fe3d88e3563c39f4fd85cecd0b6d6a
Bratet <ahmedmrabet.002@gmail.com>
2023-07-25 12:23:20 +0100

    added new feature : google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,39 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: "Ahmed Mrabet",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
-  plugins: ["gatsby-plugin-postcss","gatsby-plugin-dark-mode",   {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `assets`,
-      path: `${__dirname}/src/assets/`,
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
     },
-  },],
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-dark-mode",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+  ],
 };
